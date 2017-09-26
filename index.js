b0ec305c023fa2391a9c22b3408b331b5fd7a949
Aslan1st <aslan1st@mac.com>
2017-09-26 03:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function whisper(string) {
 }
 function logShout(string) {
   console.log(`${string}.toUpperCase()`)
-  return string
 }
 function logWhisper(string) {
   console.log(`${string}.toLowerCase()`)
