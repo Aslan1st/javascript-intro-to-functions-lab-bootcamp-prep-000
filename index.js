1c9df7fd0ef7f648e7f94b45c3243bc2d0155966
Aslan1st <aslan1st@mac.com>
2017-09-26 04:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(`${string.toLowerCase()}`)
 }
-function sayHiGrandma(string) {
+function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return ("I can't hear you!")
   else if (string === string.toUpperCase())
